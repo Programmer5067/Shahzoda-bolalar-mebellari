7c74f673a1dcebd66b100755e99e9509a95728b0
evilprog <xyz@gmail.com>
2024-09-13 19:43:21 +0500

    add new

--- connection/script.js
+++ connection/script.js
@@ -4,6 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const header = document.querySelector(".header")
     const blue = document.querySelectorAll(".blue")
 
+    if (document.body.clientWidth <= 1000) {
+        const num = document.body.querySelector(".num")
+        if (num) {
+            num.addEventListener("click", function () {
+                num.href = "tel:+998998531616"
+            })
+        }
+    }
+
     menu_btn.addEventListener("click", () => {
         document.body.classList.add("dark-body")
         document.body.classList.add("no-scroll")
